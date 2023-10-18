7418943bbbd1d07be6c89f98551242ad865461c5
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-10-18 18:25:28 -0400

    add base streamlit code

---
+++ streamlit_apps/Face_Recognition_deepface.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from deepface import DeepFace
+
+def app():
+    st.set_page_config(page_title="Face Recognition App Using Deepface")
+    st.header("Upload an image")
+    st.file_uploader("Choose an image...", type=["jpg", "png"])
+
