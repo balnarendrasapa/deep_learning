4177dca2a7198584410cbc4657ff9ef02c3d3e49
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-10-25 20:30:00 +0000

    add analyze function

--- streamlit_apps/object_detection.py
+++ streamlit_apps/object_detection.py
@@ -1,9 +1,15 @@
 import streamlit as st
-# from deepface import DeepFace
+from deepface import DeepFace
 
 def app():
     st.set_page_config(page_title="Face Recognition App Using Deepface")
     st.header("Upload an image")
     image = st.file_uploader("Choose an image...", type=["jpg", "png"])
+    if image != None:
+        with open("image.jpg", "wb") as f:
+            f.write(image.getbuffer())
+        st.image(image, use_column_width=True, width=300)
+        k = DeepFace.analyze(img_path="image.jpg", actions=["age", "gender"])
+        st.write(k)
 
 app()
