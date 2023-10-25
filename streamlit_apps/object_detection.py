d60602de310a0b42d5a66acf43b4788dbae514ea
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-10-25 20:41:16 +0000

    add model selection

--- streamlit_apps/object_detection.py
+++ streamlit_apps/object_detection.py
@@ -5,11 +5,23 @@ def app():
     st.set_page_config(page_title="Face Recognition App Using Deepface")
     st.header("Upload an image")
     image = st.file_uploader("Choose an image...", type=["jpg", "png"])
+    models = [
+        "VGG-Face", 
+        "Facenet", 
+        "Facenet512", 
+        "OpenFace", 
+        "DeepFace", 
+        "DeepID", 
+        "ArcFace", 
+        "Dlib", 
+        "SFace",
+    ]
+    model = st.selectbox("Select a model", models)
     if image != None:
         with open("image.jpg", "wb") as f:
             f.write(image.getbuffer())
-        st.image(image, use_column_width=True)
-        k = DeepFace.analyze(img_path="image.jpg", actions=["age", "gender"])
+        st.image(image)
+        k = DeepFace.analyze(img_path="image.jpg", actions=["age", "gender"], model_name=model)
         st.write(k)
 
 app()
