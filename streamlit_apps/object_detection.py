42be73b107f81d2efdda146e59b2f2cad973b6a8
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-10-25 20:42:23 +0000

    remove select_box

--- streamlit_apps/object_detection.py
+++ streamlit_apps/object_detection.py
@@ -5,12 +5,11 @@ def app():
     st.set_page_config(page_title="Face Recognition App Using Deepface")
     st.header("Upload an image")
     image = st.file_uploader("Choose an image...", type=["jpg", "png"])
-    model = st.selectbox()
     if image != None:
         with open("image.jpg", "wb") as f:
             f.write(image.getbuffer())
         st.image(image)
-        k = DeepFace.analyze(img_path="image.jpg", actions=["age", "gender"])
+        k = DeepFace.analyze(img_path="image.jpg", actions=["age", "gender", "race", "emotion"])
         st.write(k)
 
 app()
