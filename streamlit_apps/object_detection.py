b9d481b7998f8e2a7b3597c3a3fa3345ebebb1e7
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-10-25 20:41:34 +0000

    remove model selection

--- streamlit_apps/object_detection.py
+++ streamlit_apps/object_detection.py
@@ -5,23 +5,12 @@ def app():
     st.set_page_config(page_title="Face Recognition App Using Deepface")
     st.header("Upload an image")
     image = st.file_uploader("Choose an image...", type=["jpg", "png"])
-    models = [
-        "VGG-Face", 
-        "Facenet", 
-        "Facenet512", 
-        "OpenFace", 
-        "DeepFace", 
-        "DeepID", 
-        "ArcFace", 
-        "Dlib", 
-        "SFace",
-    ]
-    model = st.selectbox("Select a model", models)
+    model = st.selectbox()
     if image != None:
         with open("image.jpg", "wb") as f:
             f.write(image.getbuffer())
         st.image(image)
-        k = DeepFace.analyze(img_path="image.jpg", actions=["age", "gender"], model_name=model)
+        k = DeepFace.analyze(img_path="image.jpg", actions=["age", "gender"])
         st.write(k)
 
 app()
